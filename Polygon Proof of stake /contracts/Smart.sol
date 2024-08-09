15c9d992018fa3529c1dfdd644dabc1ce73c79d9
Sukhwinder42 <114457848+Sukhwinder42@users.noreply.github.com>
2024-08-09 17:34:22 +0530

    Update Smart.sol

--- Polygon Proof of stake /contracts/Smart.sol
+++ Polygon Proof of stake /contracts/Smart.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
@@ -8,11 +9,9 @@ contract Smart is ERC721A {
 
     uint256 public maxLimit = 5;
 
-    string baseUrl =
-    "https://pink-magic-catshark-193.mypinata.cloud/ipfs/QmUn3CRgfUaHp1uYagziR5AGxkdcdfzBTfuf5QpYWAxybT/?pinataGatewayToken=J_Sxxg4ki5e5mVU1lz4s9arwx5sh4QOM8VmMu47QBtNW02FF7EK5CLa3aovuXQLk";
-     
+    string baseUrl = "https://gateway.pinata.cloud/ipfs/QmRcX1vaGK9mBE2smwn9Mva5T3fXQhz29PfhZhEiZny8gU/";
 
-    string public prompt = "A Scary Person Illustrative style  Potrait , Fire the Background ,Lokking in Fire ";
+    string public prompt = "football player scoring a touchdown in the rain";
 
     constructor() ERC721A("Delta", "BGMI") {
         owner = msg.sender;
